b6e487d7f7e7dd649cf0cc4017ddf84e30e85b74
Harry Ng <harryworld@gmail.com>
2016-04-21 23:41:50 +0800

    Start using Carthage

--- CarthageDemo/ViewController.swift
+++ CarthageDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import SwiftDate
 
 class ViewController: NSViewController {
 
@@ -14,6 +15,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print(NSDate().day)
     }
 
     override var representedObject: AnyObject? {
